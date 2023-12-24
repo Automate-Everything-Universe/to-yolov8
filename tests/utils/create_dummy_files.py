91fd4b5995ea5ad65d8bc8f77552d5fdb61e5b4e
Dragosjosan <dragosjosan@gmail.com>
2023-12-24 10:46:17 +0100

    Fix path after refactoring Create the classes.txt and notes.json files as well

--- tests/utils/create_dummy_files.py
+++ tests/utils/create_dummy_files.py
@@ -1,8 +1,9 @@
 import shutil
+import json
 from pathlib import Path
 from typing import Union
 
-PATH_TO_DUMMY_FILES = Path(__file__).parents[0] / "yolo_format"
+PATH_TO_DUMMY_FILES = Path(__file__).parents[1] / "yolo_format"
 
 
 def create_dummy_img(file_path: Path) -> None:
@@ -25,6 +26,45 @@ def crete_dummy_label(file_path: Path) -> None:
     """
     with open(file_path, 'w') as f:
         f.write("This is a dummy label file")
+
+
+def create_dummy_classes_file(file_path: Path) -> None:
+    """
+    Creates a dummy yolo specific classes.txt file at the specified path.
+    Args:
+    file_path (Path): The path where the dummy PNG file will be created
+    """
+    content = "ElectricalOutlet\n" \
+              ""
+    classes_file = file_path / "classes.txt"
+    with open(classes_file, 'w') as f:
+        f.write(content)
+
+
+def create_dummy_notes_file(file_path: Path) -> None:
+    """
+    Creates a dummy yolo specific notes.json file at the specified path.
+    Args:
+    file_path (Path): The path where the dummy PNG file will be created
+    """
+    content = {
+        "categories": [
+            {
+                "id": 0,
+                "name": "ElectricalOutlet"
+            }
+        ],
+        "info": {
+            "year": 2023,
+            "version": "1.0",
+            "contributor": "Label Studio"
+        }
+    }
+
+    notes_file = file_path / "notes.json"
+
+    with open(notes_file, 'w', encoding='utf-8') as file:
+        json.dump(content, file, indent=4)
 
 
 def setup_dummy_files(folder_path: Union[Path, str], number_of_files: int = 100) -> None:
@@ -51,6 +91,9 @@ def setup_dummy_files(folder_path: Union[Path, str], number_of_files: int = 100)
         label_path = folder_path / "labels" / f"dummy_{i}.txt"
         create_dummy_img(file_path)
         crete_dummy_label(label_path)
+
+    create_dummy_classes_file(folder_path)
+    create_dummy_notes_file(folder_path)
 
 
 def main() -> None:
